62bbc2b110ca839379fedb80ef91824d6551b34b
Alejandro-Oficina <air5795@gmail.com>
2024-02-21 11:29:07 -0400

    cambios diseño 2

--- sistema/sub-exp-cons/obtener_registros.php
+++ sistema/sub-exp-cons/obtener_registros.php
@@ -100,8 +100,8 @@
 
 
 $fecha = '<div style="align-items: center; font-size:12px;" class="w-100">';
-$fecha .= '<div><a class="btn btn-success btn-sm w-100">Fecha de Inicio</a><br>'.$fecha_i.'</div>';
-$fecha .= '<div><a class="btn btn-danger btn-sm w-100">Fecha de Finalizacion</a><br>'.$fecha_f.'</div>';
+$fecha .= '<div><a class="btn btn-warning btn-sm disabled w-100"><strong><i class="bi bi-cone"></i> Fecha de Inicio</strong></a><br>'.$fecha_i.'</div>';
+$fecha .= '<div><a class="btn btn-success btn-sm disabled w-100"><strong><i class="bi bi-patch-check"></i> Fecha de Finalizacion</strong></a><br>'.$fecha_f.'</div>';
 $fecha .= '</div>';
 
 
